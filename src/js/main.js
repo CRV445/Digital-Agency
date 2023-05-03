68b85ecc49f1d6ff2d9741fc49fddfc91a59a3a6
Andrey Sergeevich <duhovlovec@gmail.com>
2023-05-03 22:04:50 +0700

    v.1.2 addied modal and js-code for modal

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,35 @@
 "use strict";
+
+// Modal
+const modal = document.querySelector('.modal'),
+    btnTrigger = document.querySelectorAll('[data-modal'),
+    btnClose = document.querySelector('[data-close');
+
+function showModal() {
+    modal.classList.toggle('show');
+    document.body.style.overflow = 'hidden';
+}
+
+function hideModal() {
+    modal.classList.toggle('show');
+    document.body.style.overflow = '';
+}
+
+btnTrigger.forEach(btn => {
+    btn.addEventListener('click', showModal);
+})
+
+btnClose.addEventListener('click', hideModal);
+
+modal.addEventListener('click', (e) => {
+    if(e.target === modal) {
+        modal.classList.toggle('show');
+        document.body.style.overflow = '';
+    }
+});
+
+document.addEventListener('keydown', (e) => {
+    if(e.code === 'Escape' && modal.classList.contains('show')) {
+        hideModal();
+    }
+});
